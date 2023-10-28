92d8efe46d584579c89ef84f5f818eee2218085e
traplordpanda <kdaub.tf2@gmail.com>
2023-10-28 17:21:07 -0400

    fixing test

--- tests/test_api_hash.cpp
+++ tests/test_api_hash.cpp
@@ -3,41 +3,24 @@ import api_hash;
 
 #include <gtest/gtest.h>
 #include <Windows.h>
-
-// Simple test to ensure that the functionResolver can resolve a known function hash
-TEST(FunctionResolverTest, MessageBoxHashTest) {
-    // Decide which hash function to use
-    constexpr auto hf = hash_functions::fnva1;
-
-    // compile-time hash function no string stored
-    constexpr auto msgboxhash = hf("MessageBoxA");
-
-    // init functionResolver object
-    auto fh = functionResolver{hf, "user32.dll"};
-
-    // Check if MessageBoxA's hash can be resolved
-    auto mbox_address = fh.resolve_function_hash(msgboxhash);
-    EXPECT_NE(mbox_address, nullptr);
-}
+#include <compare>
 
 // Simple test to ensure that the functionResolver can call a known function using its hash
-TEST(FunctionResolverTest, MessageBoxCallTest) {
+TEST(FunctionResolverTest, GetComputerNametest) {
     // Decide which hash function to use
     constexpr auto hf = hash_functions::fnva1;
 
     // compile-time hash function no string stored
-    constexpr auto msgboxhash = hf("MessageBoxA");
-
-    // init functionResolver object
-    auto fh = functionResolver{hf, "user32.dll"};
-
-    // Try calling MessageBoxA using its hash
-    int result = fh.call_hashed_function<int>(msgboxhash, nullptr, "hello test", "hashed api call!",
-                                              MB_OKCANCEL);
-    EXPECT_NE(result, IDCANCEL);
+    constexpr auto cname_hash = hf("GetComputerNameA");
+    EXPECT_EQ(cname_hash, 0x446eaa3c); 
+    auto fh = functionResolver{hf, "kernel32.dll"};
+    auto cname_address = fh.resolve_function_hash(cname_hash);
+    EXPECT_NE(cname_address, nullptr);
+    char cname[MAX_COMPUTERNAME_LENGTH + 1];
+    DWORD size = MAX_COMPUTERNAME_LENGTH + 1;
+	auto result = fh.call_hashed_function<bool>(cname_hash, cname, &size);
+    EXPECT_EQ(result, true);
 }
-
-// Add more tests as required...
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
